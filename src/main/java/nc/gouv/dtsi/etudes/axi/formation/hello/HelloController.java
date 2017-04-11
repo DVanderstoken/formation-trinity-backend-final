ac4b2783f83ca27c41f76ba375dbf8dbdf894f88
Didier VANDERSTOKEN <didier.vanderstoken@recif.nc>
2017-04-11 14:26:57 +1100

    feat(hello-controller): first controller with @RequestParam.

--- src/main/java/nc/gouv/dtsi/etudes/axi/formation/hello/HelloController.java
+++ src/main/java/nc/gouv/dtsi/etudes/axi/formation/hello/HelloController.java
@@ -2,9 +2,9 @@ package nc.gouv.dtsi.etudes.axi.formation.hello;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,18 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
 	private static final String helloToSomeone = "Hello ";
-	private static final String helloWorld = helloToSomeone + "World !";
 
 	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public HttpEntity<String> sayHello() {
-
-		return new HttpEntity<String>(helloWorld);
-
-	}
-
-	@RequestMapping(value = "/hello/{someone}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public HttpEntity<String> sayHelloToSomeone(
-			@PathVariable("someone") final String someone) {
+			@RequestParam(name = "someone", required = false, defaultValue = "World") final String someone) {
 
 		return new HttpEntity<String>(helloToSomeone + someone + " !");
 
