b7969674a59be45be8d157497d7214fa8bb1d7ff
Didier VANDERSTOKEN <didier.vanderstoken@recif.nc>
2017-04-11 13:50:00 +1100

    Première application / premier commit.

---
+++ src/main/java/nc/gouv/dtsi/etudes/axi/formation/FormationTrinityBackendFinalApplication.java
@@ -0,0 +1,12 @@
+package nc.gouv.dtsi.etudes.axi.formation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationTrinityBackendFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationTrinityBackendFinalApplication.class, args);
+	}
+}
