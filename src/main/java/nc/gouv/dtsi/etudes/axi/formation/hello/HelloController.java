ab4af1d3f09957b5ccee54523346063db718e57b
Didier VANDERSTOKEN <didier.vanderstoken@recif.nc>
2017-04-11 14:21:09 +1100

    feat(hello-controller): first controller with @PathVariable.

--- src/main/java/nc/gouv/dtsi/etudes/axi/formation/hello/HelloController.java
+++ src/main/java/nc/gouv/dtsi/etudes/axi/formation/hello/HelloController.java
@@ -2,21 +2,30 @@ package nc.gouv.dtsi.etudes.axi.formation.hello;
 
 import org.springframework.http.HttpEntity;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1")
 public class HelloController {
 
-	private static final String hello = "Hello World !";
+	private static final String helloToSomeone = "Hello ";
+	private static final String helloWorld = helloToSomeone + "World !";
 
-	@RequestMapping(value = "/api/v1/hello", 
-                        method = RequestMethod.GET, 
-                        produces = MediaType.APPLICATION_JSON_VALUE)
+	@RequestMapping(value = "/hello", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public HttpEntity<String> sayHello() {
 
-		return new HttpEntity<String>(hello);
+		return new HttpEntity<String>(helloWorld);
+
+	}
+
+	@RequestMapping(value = "/hello/{someone}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+	public HttpEntity<String> sayHelloToSomeone(
+			@PathVariable("someone") final String someone) {
+
+		return new HttpEntity<String>(helloToSomeone + someone + " !");
 
 	}
 
